504382de6bb90a45b138daf16ce23064dbbd5f0c
nauraadella2 <naura_adella@pnl.ac.id>
2025-04-09 12:01:13 +0700

    Initial commit

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    {{-- resources/views/admin/dashboard.blade.php --}}
+<h1>Selamat datang Admin!</h1>
+<a href="{{ route('logout') }}"
+   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+    Logout
+</a>
+<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+    @csrf
+</form>
+
+</body>
+</html>
